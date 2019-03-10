ffeeda0e36d6f12ee1d1851887abcdc2ecd08836
danielshulgin <danielshulgin@users.noreply.github.com>
2019-03-10 10:35:56 +0200

    fix

--- IA-82/3/lab_4/src/java/ServletMember.java
+++ IA-82/3/lab_4/src/java/ServletMember.java
@@ -7,15 +7,18 @@
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
 
 /**
  *
  * @author Даниил
  */
-public class NewServlet extends HttpServlet {
+@WebServlet(urlPatterns = {"/FirstServlet"})
+public class ServletMember extends HttpServlet {
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -28,19 +31,7 @@ public class NewServlet extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet NewServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet NewServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
+
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
@@ -55,6 +46,23 @@ public class NewServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+                response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>" + request.getParameter("name") + "</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<img src='Images\\" + request.getParameter("name") + ".jpg'/>");   
+            if("Daniel".equals(request.getParameter("name")))
+            {
+                out.println("<p><a href=\"https://github.com/danielshulgin\">daniel GitHub</a></p>");
+            }
+            out.println("<p><a href=\"http://localhost:8080/lab_4\">Start page</a></p>");
+            out.println("</body>");
+            out.println("</html>");
+        }
         processRequest(request, response);
     }
 
