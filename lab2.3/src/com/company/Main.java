e7bbcc2dfb58d9764ec3e86643bd7768d9041671
selezenart <47925608+selezenart@users.noreply.github.com>
2019-03-18 19:32:23 +0200

    Update Main.java

--- lab2.3/src/com/company/Main.java
+++ lab2.3/src/com/company/Main.java
@@ -1,17 +1,35 @@
 package com.company;
 
-import java.io.*;
-import java.util.*;
+        import java.io.*;
+        import java.util.*;
 
 public class Main {
-
-    public static void maxWordPrint(Map<String,Integer> wordMap){
+    public static void maxWordPrint(Map<String, Integer> wordMap){
         int max = (int) Collections.max(wordMap.values());
         Iterator it = wordMap.entrySet().iterator();
         while (it.hasNext()) {
             Map.Entry pair = (Map.Entry)it.next();
             if (pair.getValue().equals(max)){
                 System.out.println(pair.getKey() + " is repeated  " + pair.getValue()+" times");}
+
+        }
+    }
+     public static void makeThreds(File[] files,Map<String,Integer> Map){
+        int i =0;
+        Thread[] threads = new Thread[files.length];
+        for (File file:files) {
+            MyRunnable myRunnable = new MyRunnable("Thread " + (i+1), file, Map);
+            Thread thread = new Thread(myRunnable);
+            threads[i]=thread;
+            thread.start();
+            i++;
+        }
+        for (Thread thread:threads) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
 
         }
     }
@@ -36,18 +54,18 @@ public class Main {
         File myFile14 = new File("MyFile14");
         File myFile15 = new File("MyFile15");
         File myFile16 = new File("MyFile16");
-        File[] files = new File[]{myFile1,myFile2,myFile3,myFile4,myFile5,myFile6,myFile7,myFile8,
-                myFile9,myFile10,myFile11,myFile12,myFile13,myFile14,myFile15,myFile16};
+        File[] files = new File[]{
+                 myFile1,myFile2,myFile3,myFile4,myFile5,myFile6,myFile7,myFile8,
+                 myFile9,myFile10,myFile11,myFile12,myFile13,myFile14,myFile15,myFile16
+         };
 
-        MyRunnable myRunnable1 =new MyRunnable("Thread 1 ",files,wordToCount);
+         makeThreds(files,wordToCount);
 
-        Thread thread1 = new Thread(myRunnable1);
-
-        thread1.start();
-        thread1.join();
 
         maxWordPrint(wordToCount);
 
 
+
     }
 }
+
