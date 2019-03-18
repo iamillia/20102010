e19a56a8c5dc7c0675d31f301a21a90ac4240a3e
selezenart <47925608+selezenart@users.noreply.github.com>
2019-03-18 09:17:05 +0200

    Add files via upload

---
+++ lab2.3/src/com/company/Main.java
@@ -0,0 +1,53 @@
+package com.company;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void maxWordPrint(Map<String,Integer> wordMap){
+        int max = (int) Collections.max(wordMap.values());
+        Iterator it = wordMap.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry pair = (Map.Entry)it.next();
+            if (pair.getValue().equals(max)){
+                System.out.println(pair.getKey() + " is repeated  " + pair.getValue()+" times");}
+
+        }
+    }
+
+
+
+    public static void main(String[] args) throws InterruptedException {
+        Map<String, Integer> wordToCount = Collections.synchronizedMap(new HashMap<String,Integer>());
+        File myFile1 = new File("MyFile1");
+        File myFile2 = new File("MyFile2");
+        File myFile3 = new File("MyFile3");
+        File myFile4 = new File("MyFile4");
+        File myFile5 = new File("MyFile5");
+        File myFile6 = new File("MyFile6");
+        File myFile7 = new File("MyFile7");
+        File myFile8 = new File("MyFile8");
+        File myFile9 = new File("MyFile9");
+        File myFile10 = new File("MyFile10");
+        File myFile11 = new File("MyFile11");
+        File myFile12 = new File("MyFile12");
+        File myFile13 = new File("MyFile13");
+        File myFile14 = new File("MyFile14");
+        File myFile15 = new File("MyFile15");
+        File myFile16 = new File("MyFile16");
+        File[] files = new File[]{myFile1,myFile2,myFile3,myFile4,myFile5,myFile6,myFile7,myFile8,
+                myFile9,myFile10,myFile11,myFile12,myFile13,myFile14,myFile15,myFile16};
+
+        MyRunnable myRunnable1 =new MyRunnable("Thread 1 ",files,wordToCount);
+
+        Thread thread1 = new Thread(myRunnable1);
+
+        thread1.start();
+        thread1.join();
+
+        maxWordPrint(wordToCount);
+
+
+    }
+}
---
+++ lab2.3/src/com/company/MyRunnable.java
@@ -0,0 +1,69 @@
+package com.company;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MyRunnable implements Runnable {
+    String name;
+    File[] files;
+    Map<String, Integer> wordMap;
+
+    public MyRunnable(String name, File[] files, Map<String, Integer> wordMap) {
+        this.wordMap = wordMap;
+        this.name = name;
+        this.files=files;
+    }
+
+    @Override
+    public void run() {
+        long t1 = System.nanoTime()/1000;
+
+
+        for (File file : files) {
+
+            FileReader fr = null;
+            try {
+                fr = new FileReader(file);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            BufferedReader buff = new BufferedReader(fr);
+            String line = null;
+            try {
+                line = buff.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            String line1 = "";
+            while (line != null) {
+                line1 = line.toLowerCase() + " ";
+                try {
+                    line = buff.readLine();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                String[] str = line1.split(" ");
+
+                for (String word : str) {
+                    if (!wordMap.containsKey(word)) {
+                        wordMap.put(word, 0);
+                    }
+                    wordMap.put(word, wordMap.get(word) + 1);
+
+                }
+
+            }
+            try {
+                buff.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+        long t2 = System.nanoTime()/1000;
+
+        System.out.println("duration: of "+name+" is "+ (t2-t1) + " mcrs");
+
+    }
+}
