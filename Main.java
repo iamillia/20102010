3704978701db9c8c83ff2d9a6629abef12822505
Ann Kolomiets <43034693+vetoshka@users.noreply.github.com>
2019-03-24 20:04:44 +0200

    Delete Main.java

--- Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        FileFinder fileFinder = new FileFinder();
-        long currentTime = System.currentTimeMillis();
-        System.out.println(fileFinder.rarestWord("D://file1.txt", "D://file2.txt"));
-        System.out.println(System.currentTimeMillis() - currentTime);
-    }
-}
