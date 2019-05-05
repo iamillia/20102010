45c4962e5bca3352ae0cf44fbb2141580ee02946
NoThanksss <48645422+NoThanksss@users.noreply.github.com>
2019-05-05 21:44:53 +0300

    Add files via upload

---
+++ IA-81/4/lab_10/src/com/company/MyInterface.java
@@ -0,0 +1,5 @@
+package com.company;
+
+interface MyInterface<T>{
+    void calculate(T a,T b,T c,T d);
+}
---
+++ IA-81/4/lab_10/src/com/company/song.java
@@ -0,0 +1,66 @@
+package com.company;
+
+public class song implements Comparable<song> {
+    private int kachestvo;
+    private int time;
+    private String name;
+    private String author;
+    //private int numberOflooking;
+
+    public int getKachestvo() {
+        return kachestvo;
+    }
+
+    public  void setKachestvo(int kachestvo) {
+        this.kachestvo = kachestvo;
+    }
+
+    public int getTime() {
+        return time;
+    }
+
+    public void setTime(int time) {
+        this.time = time;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+
+    @Override
+    public String toString() {
+        return "song{" +
+                "kachestvo:" + kachestvo +
+                ", time:" + time +
+                ", name:'" + name + '\'' +
+                ", author:" + author;
+    }
+
+    public song(int kachestvo, int time, String name, String author) {
+        this.kachestvo = kachestvo;
+        this.author = author;
+        this.time = time;
+        this.name = name;
+    }
+
+
+    @Override
+    public int compareTo(song o) {
+        return this.time - o.time;
+    }
+
+
+}
