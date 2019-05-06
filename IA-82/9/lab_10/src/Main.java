4611f448e170ef784870ba432cad00f3d76e377c
Alex-Rogov <omorpen@gmail.com>
2019-05-06 10:41:05 +0300

    Update Main.java

--- IA-82/9/lab_10/src/Main.java
+++ IA-82/9/lab_10/src/Main.java
@@ -4,15 +4,8 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         //Part 1
-        Function function = ((double a, double b, double c, double d) -> (Math.sin(Math.abs(a)) + Math.cos(Math.sqrt(b)))/ (2 * Math.tan(c) + Math.pow(Math.E, d)));
-
-        double a = -2.98,
-                b = 5.55,
-                c = 0.045,
-                d = 0.129,
-                result = function.calculate(a, b, c, d);
-
-        System.out.printf("A = %f, B = %f, C = %f, D = %f. Result: %f\n", a, b, c, d, result);
+        printResult((double a, double b, double c, double d) -> (Math.sin(Math.abs(a)) + Math.cos(Math.sqrt(b)))/ (2 * Math.tan(c) + Math.pow(Math.E, d)),
+        -2.98, 5.55, 0.045, 0.129);
 
         //Part 2
         ArrayList<QuadraticEquation> quadraticEquations = new ArrayList(Arrays.asList(
@@ -22,6 +15,12 @@ public class Main {
                 new QuadraticEquation(2, 3, 1)));
 
         quadraticEquations.forEach(element -> System.out.println(element));
+    }
+
+    static void printResult(Function func, double a, double b, double c, double d) {
+        double result = func.calculate(a, b, c, d);
+
+        System.out.printf("A = %f, B = %f, C = %f, D = %f. Result: %f\n", a, b, c, d, result);
     }
 
     @FunctionalInterface
